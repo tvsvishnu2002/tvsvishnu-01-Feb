05d376322279386c9905612ecf86dc33672ba91d
tvsvishnu2002 <vishnuvardhantvs@backflipt.com>
2023-02-02 11:48:18 +0530

    Committing Passing Arguments File

---
+++ passing_arguments.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#Shell script for passing arguments through non - interactive method
+
+#Shell Script for Finding area of a rectangle
+
+#Check if any of the arguments is empty or not
+if [ -z $1 ] || [ -z $2 ];
+then
+    #Some arguments missing
+    echo "Some of the Arguments Missing"
+
+else
+    #Arguments are not empty. Displaying the arguments and area of the rectangle.
+    echo "Length of the rectangle is $1"
+    echo "Breadth of the rectangle is $2"
+    echo "Area of the rectangle is $(expr $1 \* $2) "
+fi
