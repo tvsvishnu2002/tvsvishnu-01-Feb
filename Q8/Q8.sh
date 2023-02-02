ea2a23411976b36f836e7943b13dd29d2caeef1d
tvsvishnu2002 <vishnuvardhantvs@backflipt.com>
2023-02-02 14:39:21 +0530

    Updated Question 8

--- Q8/Q8.sh
+++ Q8/Q8.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "Echo Enter a Directory Name"
-read direc 
-case $direc in 
+no_of_arguments_passed=$#
+if [ $no_of_arguments_passed -eq 0 ];
+then
+echo "No argument passed"
+elif [ $no_of_arguments_passed -gt 1 ];
+then
+echo "More than 1 argument passed"
+else
+case $1 in 
     [dD][eE][sS])
         ls /mnt/c/Users/tvsvi/Desktop
         ;;
@@ -10,5 +16,9 @@ case $direc in
         ;;
     [dD][oO][wW])
         ls /mnt/c/Users/tvsvi/Downloads
+        ;;
+    *)
+        echo "Enter Doc, Des or Dow"
 esac
 
+fi
